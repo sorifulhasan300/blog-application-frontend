a68ab939db7fe6aac33f75b0e6b7a7fdedc5611e
Md Soriful hasan <sorifullhasan300@gmail.com>
2026-01-23 12:07:01 +0600

    refactor(auth): remove session fetching from common layout page
    Remove the API call to fetch session data from localhost:5000/api/auth/get-session. This simplifies the page and aligns with the recent integration of better-auth for authentication handling.

---
+++ src/services/user.service.ts
@@ -0,0 +1,22 @@
+import { cookies } from "next/headers";
+
+export const userServices = {
+  getSession: async function () {
+    try {
+      const cookiesData = await cookies();
+      const res = await fetch("http://localhost:5000/api/auth/get-session", {
+        headers: {
+          Cookie: cookiesData.toString(),
+        },
+        cache: "no-store",
+      });
+      const session = await res.json();
+      if (session === null) {
+        return { data: null, error: "cookies not found" };
+      }
+      return { data: session, error: null };
+    } catch (error) {
+      return { data: null, error: "something was wrong" };
+    }
+  },
+};
